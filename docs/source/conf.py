11b1d1c1ac2c6986da1035d64d9b57bccfe5ce87
Lucky B <86569470+luckyb85@users.noreply.github.com>
2023-05-23 13:29:59 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,5 +53,5 @@ html_static_path = ['_static']
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'containers.css',
+    'docs/source/_statics/containers.css,
 ]
